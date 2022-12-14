ef6d5cdd08a9a67fa1c5a930e2db669af0b4ec95
EmilGoryachih <83202732+EmilGoryachih@users.noreply.github.com>
2022-12-14 16:15:51 +0300

    Final

---
+++ motplotlib.py
@@ -0,0 +1,50 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(488, 197)
+        MainWindow.setStyleSheet("background-color: rgb(255, 255, 255);")
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(50, -20, 421, 101))
+        font = QtGui.QFont()
+        font.setFamily("MS Sans Serif")
+        font.setPointSize(10)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label.setFont(font)
+        self.label.setStyleSheet("")
+        self.label.setObjectName("label")
+        self.horizontalSlider = QtWidgets.QSlider(self.centralwidget)
+        self.horizontalSlider.setGeometry(QtCore.QRect(80, 130, 331, 22))
+        self.horizontalSlider.setMinimum(1)
+        self.horizontalSlider.setMaximum(5)
+        self.horizontalSlider.setPageStep(1)
+        self.horizontalSlider.setOrientation(QtCore.Qt.Horizontal)
+        self.horizontalSlider.setTickPosition(QtWidgets.QSlider.TicksBelow)
+        self.horizontalSlider.setTickInterval(1)
+        self.horizontalSlider.setObjectName("horizontalSlider")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(20, 60, 571, 51))
+        self.label_2.setContextMenuPolicy(QtCore.Qt.DefaultContextMenu)
+        self.label_2.setStyleSheet("color: rgb(255, 0, 0);")
+        self.label_2.setFrameShadow(QtWidgets.QFrame.Plain)
+        self.label_2.setObjectName("label_2")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(390, 170, 75, 23))
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "Здесь вы можете выбрать качество крафики"))
+        self.label_2.setText(_translate("MainWindow", "Предупреждение: если у вас слабый компьютер выбирайте низкое "
+                                                      "качество графики"))
+        self.pushButton.setText(_translate("MainWindow", "Start"))
